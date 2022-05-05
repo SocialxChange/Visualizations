11959a7fe61cd586002c9d9746f5c990887eb32e
matias-cordova <84419768+matias-cordova@users.noreply.github.com>
2022-05-05 11:59:08 -0400

    Add files via upload

--- Dashboard Sxc/shinySxc2/global.R
+++ Dashboard Sxc/shinySxc2/global.R
@@ -12,20 +12,14 @@ library(ggthemes)
 library(leaflet)
 library(sf)
  
-load("coordenadasComunas.RData")
+#load("mapa.RData")
 data <- read_excel("data.xlsx")
-data$Lugar_geografico <- chartr("ÁÉÍÓÚ", "AEIOU", toupper(data$Lugar_geografico))
 
-map_comunas1 <- data %>% 
-              group_by(Lugar_geografico) %>% 
-                summarise(Beneficiarios = sum(Beneficiarios))
-map_comunas1 <- rename(map_comunas1, "Comuna" = "Lugar_geografico")
-
-map_comunas1 <- merge(map_comunas1, map_comunas, by = "Comuna")
 data$Image <- rep(c('http://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-01.jpg',
-                   'http://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-03.jpg',
-                   "http://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-05.jpg",
-                   "https://mujeres360.org/wp-content/uploads/2021/06/ODS-9-1024x1024.jpg",
-                   "http://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-04.jpg"),6)
-load("dfMapaDominio5.RData")
+                    'http://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-03.jpg',
+                    "http://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-05.jpg",
+                    "https://mujeres360.org/wp-content/uploads/2021/06/ODS-9-1024x1024.jpg",
+                    "http://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2016/01/S_SDG_Icons-01-04.jpg"),6)
+
+
 
